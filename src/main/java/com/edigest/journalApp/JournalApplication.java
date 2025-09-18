eaa0e4f880183d0d1e1251be748e9d51db4a1c9c
Natesh055 <singhnatesh50@gmail.com>
2025-09-18 11:44:27 +0530

    Estblished connection with MongoDB Atlas

--- src/main/java/com/edigest/journalApp/JournalApplication.java
+++ src/main/java/com/edigest/journalApp/JournalApplication.java
@@ -1,5 +1,6 @@
 package com.edigest.journalApp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JournalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JournalApplication.class, args);
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("spring.data.mongodb.uri", dotenv.get("MONGODB_URI"));
+        System.setProperty("spring.data.mongodb.database", dotenv.get("MONGO_DATABASE"));
+        SpringApplication.run(JournalApplication.class, args);
 	}
 }
